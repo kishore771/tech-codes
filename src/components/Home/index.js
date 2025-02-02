6292409a92ead18310374e715be8cf3b38317e5d
kishore771 <kishoregundelli156@gmail.com>
2025-02-02 14:51:12 +0530

    Your commit message here

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -10,6 +10,11 @@ const Home = () => {
       <div className="home-container">
         <div className="home-content">
        <h1 className='name-heading'>Hi! I'm Kishore gundelli</h1>
+       <div>
+      <div className="image-container">
+        <img src="https://i.postimg.cc/d0vKsY9n/1713697455180.jpg" alt="profile" className='profile-image'/>
+        </div>
+        </div>
          <p className='description'>
          I'm a B.com graduate in Computer applications with a strong foundation in Frontend Development.
          I specialize in HTML, CSS, JavaScript, and React.js to build responsive and dynamic web applications.
@@ -19,11 +24,6 @@ const Home = () => {
          Explore my work, and let’s connect for new opportunities! Feel free to reach out if you'd like to collaborate.
             </p>
       </div>
-      <div>
-      <div className="image-container">
-        <img src="https://i.postimg.cc/d0vKsY9n/1713697455180.jpg" alt="profile" className='profile-image'/>
-        </div>
-        </div>
       </div>
       <Footer />  {/* Include Footer */}
     </div>
